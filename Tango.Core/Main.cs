2ebd95e42834bd14bddb8b279ac489396d05d797
Greg HQ <greg@inter-concept.co.uk>
2021-11-03 08:02:32 +0000

    Add a function for launching DelProf2

--- Tango.Core/Main.cs
+++ Tango.Core/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -40,6 +41,32 @@ namespace Tango.Core
 
             Thread.Sleep(INTERVAL);
             OnTick();
+        }
+
+
+        static void LaunchDelProf()
+        {
+            // Use ProcessStartInfo class
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "C:\\Windows\\DelProf2.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = "/q /d:5 ";
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not launch the DelProf2: " + ex.Message);
+            }
         }
 
     }
