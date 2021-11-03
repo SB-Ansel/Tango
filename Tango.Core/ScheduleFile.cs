ffe1e026a6fba3d68f01f637c800c395e4779a04
Greg HQ <greg@inter-concept.co.uk>
2021-11-03 09:40:18 +0000

    Begin works on scheduled execution

---
+++ Tango.Core/ScheduleFile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tango.Core
+{
+    class ScheduleFile
+    {
+        readonly string path = "";
+        private DateTime creationDate = DateTime.Now;
+
+        public ScheduleFile(string path)
+        {
+            this.path = path;
+            creationDate = DateTime.Now;
+        }
+
+        public DateTime GetDate()
+        {
+            this.ReadDateFromFile();
+            return creationDate;
+        }
+
+        public void SetDate(DateTime date)
+        {
+            this.creationDate = date;
+            this.WriteDateToFile();
+        }
+
+        private void Create()
+        {
+            File.Create(this.path);
+            File.SetAttributes(this.path, File.GetAttributes(this.path) | FileAttributes.Hidden);
+        }
+
+        private void ReadDateFromFile()
+        {
+            try
+            {
+                this.creationDate = File.GetCreationTime(this.path);
+            }
+            catch (FileNotFoundException)
+            {
+                this.Create();
+                creationDate = DateTime.Now;
+            }
+        }
+
+        private void WriteDateToFile()
+        {
+            try
+            {
+                File.SetCreationTime(this.path, this.creationDate);
+            }
+            catch (FileNotFoundException)
+            {
+                this.Create();
+            }
+        }
+    }
+}
---
+++ Tango.Core/ScheduleManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tango.Core
+{
+    class ScheduleManager
+    {
+        private const string LAST_RUN_FILE_PATH = "last_run";
+        private const string NEXT_RUN_FILE_PATH = "next_run";
+        
+        ScheduleManager()
+        {
+            ScheduleFile lastRun = new ScheduleFile(LAST_RUN_FILE_PATH);
+            ScheduleFile nextRun = new ScheduleFile(NEXT_RUN_FILE_PATH);
+
+        }
+
+        public bool ShouldRun()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
