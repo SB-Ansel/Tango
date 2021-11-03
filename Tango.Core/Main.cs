7e842ccc8e471154145a9a069666898d3fc4c8b7
Greg HQ <greg@inter-concept.co.uk>
2021-11-03 07:51:43 +0000

    Add basic tick loop

--- Tango.Core/Main.cs
+++ Tango.Core/Main.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Tango.Core
 {
     public class Main
     {
+        const int INTERVAL = 5 * 1000; // ms
+
         public Main()
         {
             Console.WriteLine("Contructing the service...");
-            
         }
 
         ~Main()
@@ -23,12 +25,21 @@ namespace Tango.Core
         {
             Console.WriteLine("Starting the service...");
 
+            OnTick();
         }
 
         public void OnStop()
         {
             Console.WriteLine("Stopping the service...");
 
+        }
+
+        public void OnTick()
+        {
+            Console.WriteLine("Tick!");
+
+            Thread.Sleep(INTERVAL);
+            OnTick();
         }
 
     }
